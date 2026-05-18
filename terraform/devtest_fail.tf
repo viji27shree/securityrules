19249e1095049f87ad3c12924dd9df6f2e9ca4e2
Vijaya Shree (Intern) <i-vijaya.s@corestack.io>
2026-05-18 22:38:50 +0530

    dev/test

---
+++ terraform/devtest_fail.tf
@@ -0,0 +1,27 @@
+resource "azurerm_service_plan" "plan_free" {
+  name                = "plan-free"
+  location            = "East US"
+  resource_group_name = "rg-dev"
+  os_type             = "Windows"
+  sku_name            = "F1"
+}
+
+resource "azurerm_windows_web_app" "webapp_dev" {
+  name                = "webapp-dev"
+  location            = "East US"
+  resource_group_name = "rg-dev"
+  service_plan_id     = azurerm_service_plan.plan_free.id
+
+  tags = {
+    environment = "dev"
+  }
+
+  logs {
+    detailed_error_messages_enabled = true
+    failed_request_tracing_enabled  = true
+
+    http_logs {
+      storage_account_url = "https://dummy.blob.core.windows.net/"
+    }
+  }
+}
---
+++ terraform/devtest_pass1.tf
@@ -0,0 +1,27 @@
+resource "azurerm_service_plan" "plan_basic" {
+  name                = "plan-basic"
+  location            = "East US"
+  resource_group_name = "rg-dev"
+  os_type             = "Windows"
+  sku_name            = "B1"
+}
+
+resource "azurerm_windows_web_app" "webapp_dev_ok" {
+  name                = "webapp-dev-ok"
+  location            = "East US"
+  resource_group_name = "rg-dev"
+  service_plan_id     = azurerm_service_plan.plan_basic.id
+
+  tags = {
+    environment = "dev"
+  }
+
+  logs {
+    detailed_error_messages_enabled = true
+    failed_request_tracing_enabled  = true
+
+    http_logs {
+      storage_account_url = "https://dummy.blob.core.windows.net/"
+    }
+  }
+}
