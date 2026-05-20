fafec660ac90b57f0b7c8dad5282b629d5b93a0e
Vijaya Shree (Intern) <i-vijaya.s@corestack.io>
2026-05-20 20:51:48 +0530

    pnac

---
+++ terraform/fail.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "automation-fail-rg"
+  location = "East US"
+}
+
+resource "azurerm_automation_account" "example" {
+  name                = "automationfailacct"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku_name            = "Basic"
+
+  public_network_access_enabled = true
+}
