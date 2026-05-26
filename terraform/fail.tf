3f235b4ec923334111f93a64e0f4548b09a3df61
Vijaya Shree (Intern) <i-vijaya.s@corestack.io>
2026-05-26 14:50:39 +0530

    managed_identity

---
+++ terraform/fail.tf
@@ -0,0 +1,32 @@
+#fail.tf:
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-function-fail"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "funcstoragefail123"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "func-plan-fail"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_function_app" "fail" {
+  name                       = "linux-function-fail"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  service_plan_id            = azurerm_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
+
+  site_config {}
+}
