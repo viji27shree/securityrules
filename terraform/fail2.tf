9acf0cdb5eaacb30b531e51e6fb37c92676f6f37
Vijaya Shree (Intern) <i-vijaya.s@corestack.io>
2026-06-10 10:29:50 +0530

    azure_cognitive_acc_net

--- terraform/fail2.tf
+++ terraform/fail2.tf
@@ -4,4 +4,9 @@ resource "azurerm_cognitive_account" "bad" {
   resource_group_name = azurerm_resource_group.example.name
   kind                = "OpenAI"
   sku_name            = "S0"
+
+    network_acls {
+    default_action = "Allow"
+  }
+  
 }
