d98e72da35e0bb8fc27c4f7be3a1dd29dcbba20c
Vijaya Shree (Intern) <i-vijaya.s@corestack.io>
2026-06-11 12:22:10 +0530

    kubernetes

---
+++ terraform/pass.tf
@@ -0,0 +1,18 @@
+resource "azurerm_kubernetes_cluster" "pass" {
+  name                = "aks-private"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksprivate"
+
+  private_cluster_enabled = true
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
