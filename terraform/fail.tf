9c2fd66fefa1b9bdcdcee265ff0e425a9b24863a
Vijaya Shree (Intern) <i-vijaya.s@corestack.io>
2026-06-11 17:30:05 +0530

    pgsql_public_access

---
+++ terraform/fail.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "postgres-rg"
+  location = "East US"
+}
+
+resource "azurerm_postgresql_flexible_server" "fail" {
+  name                   = "postgres-fail-server"
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = azurerm_resource_group.rg.location
+
+  administrator_login    = "postgresadmin"
+  administrator_password = "Password123!"
+
+  version = "14"
+  sku_name = "B_Standard_B1ms"
+
+  storage_mb = 32768
+}
