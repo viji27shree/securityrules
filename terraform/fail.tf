93297305f20b84c9bd4696567d679acfb7fa4ee9
Vijaya Shree (Intern) <i-vijaya.s@corestack.io>
2026-06-25 12:42:42 +0530

    opentofu

--- terraform/fail.tf
+++ terraform/fail.tf
@@ -1,3 +1,4 @@
+
 resource "azurerm_postgresql_flexible_server" "fail" {
 
   authentication {
--- terraform/pass.tf
+++ terraform/pass.tf
@@ -1,3 +1,4 @@
+
 resource "azurerm_postgresql_flexible_server" "example" {
   name                = "example-postgres"
   resource_group_name = azurerm_resource_group.example.name
