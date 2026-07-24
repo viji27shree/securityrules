1e068781e5f46a70b125fb69b91085032eb21a5b
Vijaya Shree (Intern) <i-vijaya.s@corestack.io>
2026-07-24 15:04:42 +0530

    signalr

---
+++ terraform/fail.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_signalr_service" "example" {
+  name                = "tfex-signalr"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    name     = "Standard_S1"
+    capacity = 1
+  }
+
+  # Non-compliant: public network access explicitly enabled.
+  public_network_access_enabled = true
+}
